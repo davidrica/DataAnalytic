3632a8c70089a9be0c0942be8adf3003d4ea65c9
davidrica <torresdavidricardo.red@gmail.com>
2024-04-16 20:02:21 -0300

    Update Link iframe token

--- controllers/empresas.js
+++ controllers/empresas.js
@@ -42,6 +42,25 @@ const crearEmpresa = async (req, res = response )=>{
 const getEmpresas = async (req,res=response)=>{
     try {
         const empresas = await Empresa.find()
+        empresas=empresas.map((empresa)=>{
+            empresa.sideitems.map((side)=>{
+                side.link= obtenerIframe(side.key)
+                return side
+            })
+            return empresa
+        })
+        // empresas.forEach( function( empresa ) {
+        //     empresa.sideitems.forEach(function(side){
+        //         if (side.key>0){
+            
+        //             
+                    
+        //         }
+        
+                
+        //     })
+            
+        //  } )
         res.status(201).json({
             ok:true,
             empresas:empresas
