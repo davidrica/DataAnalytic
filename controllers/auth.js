0cac8dce9684418a8460276f39183656136c1e6b
davidrica <torresdavidricardo.red@gmail.com>
2024-04-29 00:17:12 -0300

    Actualizar Get empresas por Usuarios

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,160 +1,138 @@
-const {response} = require('express')
-const bcrypt = require('bcryptjs')
-const Usuario = require('../models/Usuario')
-const Empresa = require('../models/Empresa')
-const {generarJWT} = require('../helpers/jwt')
+const { response } = require("express");
+const bcrypt = require("bcryptjs");
+const Usuario = require("../models/Usuario");
+const Empresa = require("../models/Empresa");
+const { generarJWT } = require("../helpers/jwt");
 
-const crearUsuario = async (req, res = response )=>{
+const crearUsuario = async (req, res = response) => {
+  const { email, password } = req.body;
 
-    const {email,password}=req.body
-    
-    try {
-        let usuario = await Usuario.findOne({email})
+  try {
+    let usuario = await Usuario.findOne({ email });
 
-        if (usuario){
-            return res.status(400).json({
-                ok:false,
-                msg:'Email ya registrado.',
-            })    
-        }
-        
-        usuario= new Usuario(req.body)
-        
-        const salt = bcrypt.genSaltSync()
-        
-        usuario.password =bcrypt.hashSync(password,salt);
-
-
-        await usuario.save()
-        const token = await generarJWT(usuario.id,usuario.name)
-        //lasttoken para limitar quien ingreso
-        //usuario.lastToken=token
-        //usuario.save()
-        
-        res.status(201).json({
-             ok:true,
-             uid: usuario.id,
-             name: usuario.name,
-             token
-         })    
-
-    } catch (error) {
-        res.status(500).json({
-            ok:false,
-            msg:'Hable con el Administrador DB',
-
-        })
-    }
-    
-}
-
-const loginUsuario =async (req, res = response )=>{
-    const {email,password}=req.body
-    console.log({email,password})
-    try {
-        const usuario = await Usuario.findOne({email})
-
-        if (!usuario){
-            return res.status(400).json({
-                ok:false,
-                msg:'Usuario y/o Password incorrectos.',
-            })    
-        }
-
-        const validPassword = bcrypt.compareSync(password,usuario.password)
-        if (!validPassword){
-            return res.status(400).json({
-                ok:false,
-                msg:'Usuario y/o Password incorrectos...',
-            })    
-        }
-
-        const token = await generarJWT(usuario.id,usuario.name)
-
-        res.status(201).json({
-            ok:true,
-            uid: usuario.id,
-            name: usuario.name,
-            token
-        })    
-
-
-
-
-    } catch (error) {
-        res.status(500).json({
-            ok:false,
-            msg:'Hable con el Administrador DB',
-
-        })
-        
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Email ya registrado.",
+      });
     }
 
+    usuario = new Usuario(req.body);
 
-}
+    const salt = bcrypt.genSaltSync();
 
-const revalidarToken =async (req, res = response ) => {
-    
-    const {uid,name} = req;
+    usuario.password = bcrypt.hashSync(password, salt);
 
-    const token = await generarJWT(uid,name);
+    await usuario.save();
+    const token = await generarJWT(usuario.id, usuario.name);
+    //lasttoken para limitar quien ingreso
+    //usuario.lastToken=token
+    //usuario.save()
 
+    res.status(201).json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el Administrador DB",
+    });
+  }
+};
 
+const loginUsuario = async (req, res = response) => {
+  const { email, password } = req.body;
 
-    res.json({
-         ok:true,
-         uid,name,
-         token
-     })
-}
+  try {
+    const usuario = await Usuario.findOne({ email });
 
-const asignarEmpresa = async (req,res=response)=>{
-    const id = req.params.idusuario
-    const idEmpresa = req.params.idempresa
-    try {
-        const usuario = await Usuario.findById(id)
-        if (!usuario){
-            return res.status(400).json({
-                ok:false,
-                msg:"No existe usuario"
-            })
-        }
-        
-        const empresa = await Empresa.findById(idEmpresa)
-        if (!empresa){
-            return res.status(400).json({
-                ok:false,
-                msg:"No existe empresa"
-            })
-        }
-        
-        if (!usuario.empresas.includes(idEmpresa)){
-            usuario.empresas.push(empresa)
-        }
-        
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Usuario y/o Password incorrectos.",
+      });
+    }
 
-                
-        
-        
-        
-        
-        const usuarioActualizado = await Usuario.findByIdAndUpdate(id,usuario,{new:true})
+    const validPassword = bcrypt.compareSync(password, usuario.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Usuario y/o Password incorrectos...",
+      });
+    }
 
-        
+    const token = await generarJWT(usuario.id, usuario.name);
 
-        res.status(200).json({
-            ok:true,
-            usuarioActualizado
-        })    
-    } catch (error) {
-        console.log(error)
-    }   
-    
-}
+    res.status(201).json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el Administrador DB",
+    });
+  }
+};
+
+const revalidarToken = async (req, res = response) => {
+  const { uid, name } = req;
+
+  const token = await generarJWT(uid, name);
+
+  res.json({
+    ok: true,
+    uid,
+    name,
+    token,
+  });
+};
+
+const asignarEmpresa = async (req, res = response) => {
+  const id = req.params.idusuario;
+  const idEmpresa = req.params.idempresa;
+  try {
+    const usuario = await Usuario.findById(id);
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "No existe usuario",
+      });
+    }
+
+    const empresa = await Empresa.findById(idEmpresa);
+    if (!empresa) {
+      return res.status(400).json({
+        ok: false,
+        msg: "No existe empresa",
+      });
+    }
+
+    if (!usuario.empresas.includes(idEmpresa)) {
+      usuario.empresas.push(empresa);
+    }
+
+    const usuarioActualizado = await Usuario.findByIdAndUpdate(id, usuario, {
+      new: true,
+    });
+
+    res.status(200).json({
+      ok: true,
+      usuarioActualizado,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
-    crearUsuario,
-    revalidarToken,
-    loginUsuario,
-    asignarEmpresa
-}
+  crearUsuario,
+  revalidarToken,
+  loginUsuario,
+  asignarEmpresa,
+};
--- controllers/empresas.js
+++ controllers/empresas.js
@@ -1,124 +1,111 @@
-const {response} = require('express')
-const bcrypt = require('bcryptjs')
-const Empresa= require('../models/Empresa')
-const {generarJWT, obtenerIframe} = require('../helpers/jwt')
+const { response } = require("express");
+const bcrypt = require("bcryptjs");
+const Empresa = require("../models/Empresa");
+const { generarJWT, obtenerIframe } = require("../helpers/jwt");
+const Usuario = require("../models/Usuario");
 
+const crearEmpresa = async (req, res = response) => {
+  const { cuit } = req.body;
 
+  try {
+    let empresa = await Empresa.findOne({ cuit });
 
-const crearEmpresa = async (req, res = response )=>{
-        const {cuit}= req.body
+    if (empresa) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Cuit ya registrado",
+      });
+    }
+    empresa = new Empresa(req.body);
 
-        try {
-            let empresa = await Empresa.findOne({cuit})
+    await empresa.save();
 
-            if (empresa){
-                return res.status(400).json({
-                    ok:false,
-                    msg:"Cuit ya registrado"
-                })
-            }
-            empresa = new Empresa(req.body)
+    res.status(201).json({
+      ok: true,
+      empresa: empresa,
+    });
+  } catch (error) {
+    res.status(201).json({
+      ok: true,
+      msg: error,
+    });
+  }
+};
 
-            await empresa.save()
+const getEmpresas = async (req, res = response) => {
+  const { uid } = req;
+  try {
+    const usuario = await Usuario.findById(uid);
 
-            res.status(201).json({
-                ok:true,
-                empresa:empresa
-            })    
-               
-        } catch (error) {
-            res.status(201).json({
-                ok:true,
-                msg:error
-            })    
-            
-        }
+    let empresas = await Empresa.find().where("_id").in(usuario.empresas);
 
+    empresas.forEach((emp) => {
+      emp.sideitems.forEach((side) => {
+        side.link = obtenerIframe(side.key);
+      });
+    });
 
+    res.status(201).json({
+      ok: true,
+      empresas: empresas,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const agregarSideItem = async (req, res = response) => {
+  const { cuit } = req.body;
 
-    
-}
+  try {
+    let empresa = await Empresa.findOne({ cuit });
+    let { titulo, link, key } = req.body;
 
-const getEmpresas = async (req,res=response)=>{
-    try {
-        let empresas = await Empresa.find()
-        empresas.forEach((emp)=>{
-            emp.sideitems.forEach((side)=>{
-                side.link=obtenerIframe(side.key);
-            })
-        })
-        
-        res.status(201).json({
-            ok:true,
-            empresas:empresas
-        })    
-            
-    } catch (error) {
-        console.log(error)
-        
+    if (!empresa) {
+      return res.status(400).json({
+        ok: false,
+        msg: "La Empresa no Existe",
+      });
     }
 
-
-}
-const agregarSideItem = async (req,res=response)=>{
-
-    const {cuit}= req.body
-    
-    try {
-        let empresa = await Empresa.findOne({cuit})
-        let {titulo,link,key} = req.body
-        
-
-        if (!empresa){
-            return res.status(400).json({
-                ok:false,
-                msg:"La Empresa no Existe"
-            })
-        }
-        
-        if (key>0){
-            
-            link = obtenerIframe(key)
-            
-        }
-        
-        const sideitem ={titulo,link,key}
-
-        const existe = empresa.sideitems.find((side) => side.titulo === titulo || side.link === link || side.key === key );
-        if (existe) {
-            return res.status(400).json({
-                ok:false,
-                msg:"Ya existe el titulo o Link "
-            })
-
-            
-        }
-        
-        
-        empresa.sideitems.push(sideitem)
-        
-        const empresaActualizado = await Empresa.findByIdAndUpdate(empresa.id,empresa,{new:true})
-
-        res.status(201).json({
-            ok:true,
-            empresaActualizado
-        })    
-           
-    } catch (error) {
-        console.log(error)
-        res.status(201).json({
-            ok:true,
-            msg:error
-        })    
-        
+    if (key > 0) {
+      link = obtenerIframe(key);
     }
 
+    const sideitem = { titulo, link, key };
 
-}
+    const existe = empresa.sideitems.find(
+      (side) => side.titulo === titulo || side.link === link || side.key === key
+    );
+    if (existe) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Ya existe el titulo o Link ",
+      });
+    }
 
- 
+    empresa.sideitems.push(sideitem);
+
+    const empresaActualizado = await Empresa.findByIdAndUpdate(
+      empresa.id,
+      empresa,
+      { new: true }
+    );
+
+    res.status(201).json({
+      ok: true,
+      empresaActualizado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({
+      ok: true,
+      msg: error,
+    });
+  }
+};
+
 module.exports = {
-    crearEmpresa,
-    agregarSideItem,
-    getEmpresas
-}
+  crearEmpresa,
+  agregarSideItem,
+  getEmpresas,
+};
